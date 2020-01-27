f649825840f1ad50b3a7725839921c4c2c20a506
Luis Sandoval <luissandoval1126@gmail.com>
2020-01-26 16:46:05 -0800

    for real this time

---
+++ assets/questions.js
@@ -0,0 +1,38 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "Intervals are set in ____.",
+      choices: ["Years", "Seconds", "MilliSeconds", "PikaSeconds"],
+      answer: "MilliSeconds"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+   
+    {
+      title: " Objects in JavaScript begin with ____.",
+      choices: ["$", ".", "#", "Uppercase Letters"],
+      answer: "Uppercase Letters"
+    },
+   
+    {
+      title: "A callback is ____.",
+      choices: ["when an actor gets a gig", "when a function takes a function as in input", "when you call a function", "a synchronous programming style"],
+      answer: "when a function takes a function as in input"
+    },
+   
+    {
+      title: " What is the correct Javascript Syntax to change the text of this HTML element? < p id = 'test'> I'm testing you! </p>",
+      choices: ["localStorage.match('test').textcontent('this is how')", "document.getElementByName('Test').innerHTML('No like this')", "document.getElementById('test').innerHTML = 'this chose this'", "$('test').attr('innerHTML', 'this)"],
+      answer: "document.getElementById('test').innerHTML = 'this chose this'"
+    },
+   
+
+   
+  ];
---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var test = document.getElementById('test')
+var fuck = localStorage.match('fuck')
+
+
+test.addEventListener('click', function(){
+    
+alert('hmmm')
+localStorage.setItem('test1', 'Fuck')
+localStorage.setItem('fuck', 'Fuck')
+alert("fuck all")}
+    
+)
